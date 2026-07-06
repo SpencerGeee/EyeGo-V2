b22b6ef685679021fcb1330a8fc052cb77dac4c1
Cyril Spencer <cyrilspencer12345@gmail.com>
2026-07-06 15:50:27 +0000

    fix: stale Hermes bytecode cache causing EXC_BAD_ACCESS on iOS startup

--- apps/rider/babel.config.js
+++ apps/rider/babel.config.js
@@ -1,5 +1,8 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
+  // If you hit Hermes bytecode corruption (EXC_BAD_ACCESS in arrayPrototypeMap
+  // on startup after JS changes), fix with:
+  //   npx expo start -c && cd ios && rm -rf build && pod deintegrate && pod install;
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
